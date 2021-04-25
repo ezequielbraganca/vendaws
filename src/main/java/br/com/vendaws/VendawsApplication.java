e28dba1a69c4ad95f67d519f4d784c241b85f241
ThermasFontes <83178247+ThermasFontes@users.noreply.github.com>
2021-04-25 16:10:37 -0300

    Comentario main

--- src/main/java/br/com/vendaws/VendawsApplication.java
+++ src/main/java/br/com/vendaws/VendawsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VendawsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VendawsApplication.class, args);
+		SpringApplication.run(VendawsApplication.class, args);	//teste
 	}
 
 }
